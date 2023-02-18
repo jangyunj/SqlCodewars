6f2a055e30f94f9d6486fd01161e30461d289513
Yun Jang <jang.yunj7@gmail.com>
2023-02-17 22:56:55 -0500

    initial commit

---
+++ SqlCodewars/Program.cs
@@ -0,0 +1,13 @@
+﻿1.select name, age from users where age >= 18;
+
+2.select price* amount as total from items;
+
+3.select number * -1 as res from opposite;
+
+4.select * from companies order by employees desc;
+
+5.select name from students where tuition_received = false;
+
+6.select * from travelers where country not in ('USA', 'Canada', 'Mexico');
+
+
